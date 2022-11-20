91471318a38d7b6e842a8c9e2c77249f4af6859e
Marina Oliveira Rocha <marina.oliveira@cubos.academy>
2022-11-20 19:38:37 -0300

    fix: dropdown bug

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -13,14 +13,13 @@ export default function Dropdown({
   setDropdown,
 }: IDropdown) {
   return (
-    <ul className={`${styles.dropdown} ${dropdown ? styles.show : null}`}>
+    <ul
+      className={`${styles.dropdown} ${dropdown ? styles.show : null}`}
+      onMouseLeave={() => setDropdown(!dropdown)}
+    >
       {submenus?.map((submenu: Submenu, index: number) => {
         return (
-          <li
-            key={index}
-            className={styles["menu-item"]}
-            onMouseLeave={() => setDropdown(!dropdown)}
-          >
+          <li key={index} className={styles["menu-item"]}>
             <a href={submenu.url}>{submenu.title}</a>
           </li>
         );
