29b8c275b81f50d64b5d86d8dfafb71102af4485
Marina Oliveira Rocha <marina.oliveira@cubos.academy>
2022-11-20 23:43:04 -0300

    feat: types

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,30 @@ export type Submenu = {
   title: string;
   url: string;
 };
+
+export type Content = {
+  title: string;
+  cover: string;
+  description: string;
+  tags: string[];
+  goodreads?: string;
+  amazon?: string;
+  itch?: string;
+  more?: string;
+  slug?: string;
+};
+
+export type ExpandedContent = {
+  title: string;
+  slug: string;
+  itch: string;
+  description: string[];
+  devblog: string;
+  gallery: string[];
+};
+
+export type Project = {
+  title: string;
+  description: string;
+  url: string;
+};
